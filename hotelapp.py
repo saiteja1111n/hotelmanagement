78ef40d481e37d34e18ebc6a57fe1e959cd7cbed
saiteja1111n <saiteja1111n@gmal.com>
2015-11-06 15:16:09 +0530

    created backend tables Room and Person

--- hotelapp.py
+++ hotelapp.py
@@ -22,16 +22,52 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 class Room(ndb.Model):
-    number = ndb.IntegerProperty(indexed =True)
+    number = ndb.StringProperty(indexed =True)
     status = ndb.StringProperty(indexed = True)
-    bookedperson = ndb.StructuredProperty(indexed=True)
-    numberofpeople = ndb.IntegerProperty(indexed=True)
-    details = ndb.StringProperty(indexed=False)
+    type = ndb.IntegerProperty(indexed=True)
+    acstatus = ndb.BooleanProperty(indexed=True)
+    cost = ndb.IntegerProperty(indexed=True)
 
 class Person(ndb.Model):
-    name = ndb.StringProperty(indexed= False)
+    roomsbooked = ndb.StructuredProperty(Room,repeated=True)
     mailId = ndb.StringProperty(indexed=True)
     phno = ndb.StringProperty(indexed= False)
-    address=ndb.StringProperty(indexed = False)
-    type = ndb.StringProperty(indexed=True)
+    Name = ndb.StringProperty(indexed = False)
 
+
+class indexPage(webapp2.RequestHandler):
+    def get(self):
+        que1 = Room.query().count()
+        if que1 == 0:
+            for i in range(1,4):
+                for j in range(1,5):
+                    Room(number=str(i)+str(j),status="available",type=i,acstatus=True,cost=1000*i).put()
+        template=JINJA_ENVIRONMENT.get_template('index1.html')
+        self.response.write(template.render())
+
+class getavailableRooms(webapp2.RequestHandler):
+    def post(self):
+        avaiable_rooms = Room.query(Room.status == "available")
+        single_room_list = []
+        double_room_list = []
+        triple_room_list = []
+        for q in avaiable_rooms:
+            if q.type == 1:
+                s = {'number':q.number,'acstatus':q.acstatus,'cost':q.cost}
+                single_room_list.append(s)
+            elif q.type == 2:
+                s = {'number':q.number,'acstatus':q.acstatus,'cost':q.cost}
+                double_room_list.append(s)
+            else:
+                s = {'number':q.number,'acstatus':q.acstatus,'cost':q.cost}
+                triple_room_list.append(s)
+        total_rooms = {"single_rooms":single_room_list,"double_rooms":double_room_list,"triple_rooms":triple_room_list}
+        total_rooms = json.dumps(total_rooms)
+        self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
+        self.response.write(total_rooms)
+
+
+app = webapp2.WSGIApplication([
+    ('/', indexPage),
+    ('/availablerooms', getavailableRooms)
+], debug=True)
