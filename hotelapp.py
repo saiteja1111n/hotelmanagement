cbac3b4f69bbc321129c3aff5b890ffbbbdd5e43
saiteja1111n <saiteja1111n@gmal.com>
2015-12-14 03:00:42 +0530

    Changed the classes in database merging Person class with Room class and changed that are needed

--- hotelapp.py
+++ hotelapp.py
@@ -22,11 +22,11 @@ JINJA_ENVIRONMENT = jinja2.Environment(
     autoescape=True)
 
 class Person(ndb.Model):
-    roomsbooked = ndb.StringProperty(repeated=True)
+    # roomsbooked = ndb.StringProperty(repeated=True)
     mailId = ndb.StringProperty(indexed=True)
     phno = ndb.StringProperty(indexed= False)
     Name = ndb.StringProperty(indexed = False)
-    bookingstatus = ndb.BooleanProperty(indexed = False,default=False)
+    # bookingstatus = ndb.BooleanProperty(indexed = False,default=False)
 
 class Room(ndb.Model):
     number = ndb.StringProperty(indexed =True)
@@ -34,6 +34,9 @@ class Room(ndb.Model):
     type = ndb.IntegerProperty(indexed=True)
     acstatus = ndb.BooleanProperty(indexed=True)
     cost = ndb.IntegerProperty(indexed=True)
+    frm_date = ndb.DateTimeProperty()
+    to_date = ndb.DateTimeProperty()
+    customer = ndb.StructuredProperty(Person)
 
 class Admin(ndb.Model):
     Name = ndb.StringProperty(indexed = True)
@@ -41,6 +44,7 @@ class Admin(ndb.Model):
 
 class Userfeedbacks(ndb.Model):
     user = ndb.StructuredProperty(Person)
+    rating = ndb.StringProperty(indexed = False)
     message = ndb.StringProperty(indexed = False)
     status = ndb.StringProperty(indexed = True)
     date = ndb.DateTimeProperty(auto_now_add=True)
@@ -85,17 +89,19 @@ class confirmrequest(webapp2.RequestHandler):
         vals = json.loads(cgi.escape(self.request.body))
         room_numbers = vals['booked_rooms'].split(',')
         room_list=[]
-        p=Person(Name=vals['person_name'],mailId=vals['person_email'],phno=vals['person_mobileno'],roomsbooked= [])
         for s in room_numbers:
             s = s.strip(' \t\n\r')
-            p.roomsbooked.append(s)
             q1 = Room.query(Room.number==s).get()
+            p=Person(Name=vals['person_name'],mailId=vals['person_email'],phno=vals['person_mobileno'])
             if q1:
+                q1.customer=p
+                q1.frm_date = datetime.strptime(vals['start_date'], "%Y-%m-%d")
+                q1.to_date =datetime.strptime(vals['end_date'], "%Y-%m-%d")
                 q1.status="awaiting"
                 q1.put()
-        p.put()
-        # Person(Name=vals['person_name'],mailId=vals['person_email'],phno=vals['person_mobileno'],roomsbooked= room_list).put()
-        message = {"message":"success"}
+                message = {"message":"success"}
+            else:
+                message = {"message":"failure","error":"could not find the room that is booked in database"}
         data = json.dumps(message)
         self.response.headers.add_header('content-type', 'application/json', charset='utf-8')
         self.response.write(data)
@@ -109,7 +115,7 @@ class contactpage(webapp2.RequestHandler):
 class savefeedback(webapp2.RequestHandler):
     def post(self):
         vals = json.loads(cgi.escape(self.request.body))
-        f = Userfeedbacks(user=Person(Name=vals['name'], mailId=vals['email'], phno=vals['phone'], roomsbooked=[]), status="unread", message = vals['msg'])
+        f = Userfeedbacks(user=Person(Name=vals['name'], mailId=vals['email'], phno=vals['phone']), status="unread", message = vals['msg'], rating=vals['rating'])
         f.put()
         message = {"message":"success"}
         data = json.dumps(message)
@@ -130,13 +136,13 @@ class adminpage(webapp2.RequestHandler):
                     fl1.append(f)
                 else:
                     fl2.append(f)
-            p1 =Person.query().fetch()
+            p1 = Room.query().fetch()
             bl1=[]
             bl2=[]
             for p in p1:
-                if not p.bookingstatus:
+                if p.status == "awaiting":
                     bl1.append(p)
-                else:
+                elif p.status == "confirmed":
                     bl2.append(p)
             template_values = {
                 'user': user,
@@ -149,6 +155,14 @@ class adminpage(webapp2.RequestHandler):
         else:
             self.redirect(users.create_login_url(self.request.uri))
 
+# class confirm_room(webapp2.RequestHandler):
+#     def post(self):
+#         vals = json.loads(cgi.escape(self.request.body))
+#         user = Person.query(Person.Name==vals['customer_name']).get()
+#         if user:
+
+
+
 
 
 app = webapp2.WSGIApplication([
@@ -158,4 +172,5 @@ app = webapp2.WSGIApplication([
     ('/contact',contactpage),
     ('/feedback',savefeedback),
     ('/admin', adminpage),
+    # ('/confirm_rm',confirm_room),
 ], debug=True)
--- js/userjs.js
+++ js/userjs.js
@@ -91,74 +91,89 @@
 
     function proceed()
     {
-        single_rooms = $("#available_single_rooms").val()
-        double_rooms = $("#available_double_rooms").val()
-        triple_rooms = $("#available_triple_rooms").val()
-        s="";
-        if(single_rooms!=null) {
-            for(var i=0;i<single_rooms.length;i++)
-                s=s+single_rooms[i]+",";
+        var start_date = document.getElementById("startdate");
+        var end_date = document.getElementById("enddate");
+        if(validate_dates(start_date.value,end_date.value)) {
+            single_rooms = $("#available_single_rooms").val()
+            double_rooms = $("#available_double_rooms").val()
+            triple_rooms = $("#available_triple_rooms").val()
+            s="";
+            if(single_rooms!=null) {
+                for(var i=0;i<single_rooms.length;i++)
+                    s=s+single_rooms[i]+",";
+            }
+             if(double_rooms!=null) {
+                for(var i=0;i<double_rooms.length;i++)
+                    s=s+double_rooms[i]+",";
+             }
+             if(triple_rooms!=null) {
+                for(var i=0;i<triple_rooms.length;i++)
+                    if((i+1)==triple_rooms.length)
+                        s=s+triple_rooms[i];
+                    else
+                        s=s+triple_rooms[i]+",";
+             }
+             rooms=s;
+             if(s!="") {
+                document.getElementById("allrooms").innerHTML=s;
+                var divbar12=document.getElementById("model1");
+                divbar12.style.display='none';
+
+                var divbar1=document.getElementById("ConfirmationPage");
+                divbar1.style.display='none';
+
+                var divbar21=document.getElementById("personal_details");
+                divbar21.style.display='block';
+
+                document.getElementById('proceed').style.visibility = 'hidden';
+                document.getElementById('back').style.visibility = 'visible';
+                document.getElementById('confirm').style.visibility = 'visible';
+
+                $('.modal-title').text('Personal Details');
+             }
+             else {
+                alert("please select atleast one room");
+             }
+        } else {
+            start_date.focus();
         }
-         if(double_rooms!=null) {
-            for(var i=0;i<double_rooms.length;i++)
-                s=s+double_rooms[i]+",";
-         }
-         if(triple_rooms!=null) {
-            for(var i=0;i<triple_rooms.length;i++)
-                if((i+1)==triple_rooms.length)
-                    s=s+triple_rooms[i];
-                else
-                    s=s+triple_rooms[i]+",";
-         }
-         rooms=s;
-         if(s!="") {
-            document.getElementById("allrooms").innerHTML=s;
-            var divbar12=document.getElementById("model1");
-            divbar12.style.display='none';
-
-            var divbar1=document.getElementById("ConfirmationPage");
-            divbar1.style.display='none';
-
-            var divbar21=document.getElementById("personal_details");
-            divbar21.style.display='block';
-
-            document.getElementById('proceed').style.visibility = 'hidden';
-            document.getElementById('back').style.visibility = 'visible';
-            document.getElementById('confirm').style.visibility = 'visible';
-
-            $('.modal-title').text('Personal Details');
-         }
-         else {
-            alert("please select atleast one room");
-         }
     }
     function ConfirmationDone()
     {
         console.log("Rooms booked"+rooms);
-        var data1 = {
-            'booked_rooms':rooms,
-            'person_name':document.getElementById("person_name").value,
-            'person_email':document.getElementById("person_email").value,
-            'person_mobileno':document.getElementById("person_mobileno").value,
-        };
-        var data = JSON.stringify(data1);
-        $.post("/conformrequest",data)
-        .done(function(data){
-            console.log("success");
-            console.log(data);
-            document.getElementById('back').style.visibility = 'hidden';
-            $('.modal-title').text('Room Booked');
-            document.getElementById('confirm').style.visibility = 'hidden';
+        var name = document.getElementById("person_name").value;
+        var email = document.getElementById("person_email");
+        var mobileno = document.getElementById("person_mobileno");
+        var start_date = document.getElementById("startdate").value;
+        var end_date = document.getElementById("enddate").value;
+        if(validate_email(email)&&validate_mobile(mobileno)){
+            var data1 = {
+                'booked_rooms':rooms,
+                'person_name':document.getElementById("person_name").value,
+                'person_email':document.getElementById("person_email").value,
+                'person_mobileno':document.getElementById("person_mobileno").value,
+                'start_date':document.getElementById("startdate").value,
+                'end_date':document.getElementById("enddate").value,
+            };
+            var data = JSON.stringify(data1);
+            $.post("/conformrequest",data)
+            .done(function(data){
+                console.log("success");
+                console.log(data);
+                document.getElementById('back').style.visibility = 'hidden';
+                $('.modal-title').text('Room Booked');
+                document.getElementById('confirm').style.visibility = 'hidden';
 
-            var divbar1=document.getElementById("ConfirmationPage");
-            divbar1.style.display='block';
+                var divbar1=document.getElementById("ConfirmationPage");
+                divbar1.style.display='block';
 
-            var divbar=document.getElementById("model1");
-            divbar.style.display='none';
+                var divbar=document.getElementById("model1");
+                divbar.style.display='none';
 
-            var divbar21=document.getElementById("personal_details");
-            divbar21.style.display='none';
-        });
+                var divbar21=document.getElementById("personal_details");
+                divbar21.style.display='none';
+            });
+        }
     }
 
     $(document).ready(function() {
@@ -167,34 +182,125 @@
 
     function feedback()
     {
-        var rating = document.getElementById("user_rating").value;
+        var rating = document.getElementById("user_rating");
 
         var name=document.getElementById("name").value;
 
-        var email=document.getElementById("email").value;
+        var email=document.getElementById("email");
 
         var msg=document.getElementById("msg").value;
 
-        var phone = document.getElementById("phone").value;
+        var phone = document.getElementById("phone");
 
-        data = {
-            'rating':rating,
-            'name':name,
-            'email':email,
-            'msg':msg,
-            'phone':phone,
+        if(!(rating.value.match(/^[0-5]([.][0-9]+)?$/))){
+            alert("Give valid rating");
+            rating.focus();
+            return;
         }
+         var x=parseFloat(rating.value);
+        if(!(0<=x<=5)){
+                alert("Give valid rating");
+                rating.focus();
+                return;
+        }
+        if(name=""){
+            alert("Please fill the Name Field");
+        }
+        if(validate_email(email)&&validate_mobile(phone)) {
 
-        data = JSON.stringify(data)
-        $.post("/feedback",data)
-        .done(function(data){
-            console.log("success");
-            console.log(data);
-            alert("Feedback Successfully submitted");
-            window.location.assign("/.");
+            data = {
+                'rating':rating.value,
+                'name':name,
+                'email':email.value,
+                'msg':msg,
+                'phone':phone.value,
+            }
 
-        });
+            data = JSON.stringify(data)
+            $.post("/feedback",data)
+            .done(function(data){
+                console.log("success");
+                console.log(data);
+                alert("Feedback Successfully submitted");
+                window.location.assign("/.");
+
+            });
+        }
     }
 
-    function ready(){
+//    function bookRoom(x,y){
+//        data={
+//            'roomno':x,
+//            'customer_name'y,
+//        }
+//        data = JSON.stringify(data);
+//        $.post("/confirm_rm",data)
+//        .done(function(data){
+//            console.log("success");
+//            console.log(data);
+//            alert("Feedback Successfully submitted");
+//            window.location.assign("/.");
+//
+//        });
+//    }
+
+    function validate_mobile(fld) {
+         if (fld.value == "") {
+          alert("You didn't enter a phone number.");
+          fld.value = "";
+          fld.focus();
+          return false;
+         }
+          else if (isNaN(fld.value)) {
+          alert("The phone number contains illegal characters.");
+          fld.value = "";
+          fld.focus();
+          return false;
+         }
+         else if (!(fld.value.length == 10)) {
+          alert("The phone number is the wrong length. \nPlease enter 10 digit mobile no.");
+          fld.value = "";
+          fld.focus();
+          return false;
+         }
+         else
+           return true;
+    }
+
+    function validate_email(tbEMObj) {
+        with (tbEMObj) {
+            apos = value.indexOf("@");
+            dotpos = value.lastIndexOf(".");
+            if (value == '') {
+                alert('field can not be blank');
+                tbEMObj.focus()
+                return false;
+            }
+            else if (apos < 1 || dotpos - apos < 2) {
+                alert('please enter correct email');
+                tbEMObj.focus()
+                return false;
+            }
+        }
+        return true;
+    }
+
+    function validate_dates(x,y) {
+        var date = x.substring(0, 2);
+        var month = x.substring(3, 5);
+        var year = x.substring(6, 10);
+        var date1 = y.substring(0, 2);
+        var month1 = y.substring(3, 5);
+        var year1 = y.substring(6, 10);
+        var startdate = new Date(x);
+        var enddate= new Date(y);
+        var currentDate = new Date();
+        if(currentDate>=startdate) {
+            alert("The start date should be after the current date");
+            return false;
+        } else if(enddate<=startdate) {
+            alert("The end date should be after startdate");
+            return false;
+        }
+        return true;
     }
